4f8c92a9344e5f0168b0f98521b0dddc74e4d8c1
magicliang <liangchuan@meituan.com>
2020-04-16 20:14:19 +0800

    First commit

---
+++ src/main/java/com/magicliang/springexperiments/SpringExperimentsApplication.java
@@ -0,0 +1,13 @@
+package com.magicliang.springexperiments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExperimentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExperimentsApplication.class, args);
+	}
+
+}
